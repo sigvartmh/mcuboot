1517ca23c73c91ed820d1d5e8e7e3cb3f3db8aa4
Sigvart Hovland <sigvart.m@gmail.com>
2019-02-25 14:06:33 +0100

    zephyr: Add log config fallback if not defined for MCUBOOT
    Signed-off-by: Sigvart Hovland <sigvart.m@gmail.com>

--- boot/zephyr/include/mcuboot_config/mcuboot_logging.h
+++ boot/zephyr/include/mcuboot_config/mcuboot_logging.h
@@ -13,9 +13,9 @@
  * When building for targets running Zephyr, delegate to its native
  * logging subsystem.
  */
-
-#define MCUBOOT_LOG_MODULE_DECLARE(domain)	LOG_MODULE_DECLARE(domain, CONFIG_MCUBOOT_LOG_LEVEL)
-#define MCUBOOT_LOG_MODULE_REGISTER(domain)	LOG_MODULE_REGISTER(domain, CONFIG_MCUBOOT_LOG_LEVEL)
+#ifndef CONFIG_MCUBOOT_LOG_LEVEL
+#define  CONFIG_MCUBOOT_LOG_LEVEL 0
+#endif
 
 #define MCUBOOT_LOG_ERR(...) LOG_ERR(__VA_ARGS__)
 #define MCUBOOT_LOG_WRN(...) LOG_WRN(__VA_ARGS__)
@@ -23,6 +23,8 @@
 #define MCUBOOT_LOG_DBG(...) LOG_DBG(__VA_ARGS__)
 
 #include <logging/log.h>
+#define MCUBOOT_LOG_MODULE_DECLARE(domain)	LOG_MODULE_DECLARE(domain, CONFIG_MCUBOOT_LOG_LEVEL)
+#define MCUBOOT_LOG_MODULE_REGISTER(domain)	LOG_MODULE_REGISTER(domain, CONFIG_MCUBOOT_LOG_LEVEL)
 
 #endif /* !__BOOTSIM__ */
 
